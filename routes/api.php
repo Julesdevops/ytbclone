dee654b6550b5923a931c01924037fa92bb2ff19
julesdevops <julesbourdais7198@gmail.com>
2020-09-17 14:35:57 +0200

    feat: define applications's base api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('search', [SearchController::class, 'getSearchResults']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+
+Route::prefix('video')->group(function () {
+    Route::get('overview', [VideoRetrieveController::class, 'getVideoOverviews']);
+    Route::get('{id}', [VideoRetrieveController::class, 'getVideo']);
+
+    Route::middleware('auth')->group(function () {
+        Route::post('{id}/like', [VideoSocialController::class, 'like']);
+        Route::post('{id}/unlike', [VideoSocialController::class, 'unlike']);
+        Route::post('{id}/dislike', [VideoSocialController::class, 'dislike']);
+        Route::post('{id}/undislike', [VideoSocialController::class, 'undislike']);
+    });
+});
+
+Route::prefix('comment')->group(function () {
+    Route::get('{id}/{offset}', [CommentRetrieveController::class, 'getVideoComments']);
+    Route::get('{id}', [CommentRetrieveController::class, 'getComment']);
+
+    Route::middleware('auth')->group(function () {
+        Route::post('add/{id}', [CommentManageController::class, 'add']);
+        Route::post('{id}/edit', [CommentManageController::class, 'edit']);
+        Route::post('{id}/delete', [CommentManageController::class, 'delete']);
+        Route::post('{id}/answer', [CommentSocialController::class, 'answer']);
+        Route::post('{id}/like', [CommentSocialController::class, 'like']);
+        Route::post('{id}/unlike', [CommentSocialController::class, 'unlike']);
+        Route::post('{id}/dislike', [CommentSocialController::class, 'dislike']);
+        Route::post('{id}/undislike', [CommentSocialController::class, 'undislike']);
+    });
+});
+
+Route::prefix('channel')->group(function () {
+    Route::get('{id}', [ChannelRetrieveController::class, 'getChannelOverview']);
+    
+    Route::middleware('auth')->group(function () {
+        Route::get('subscribed', [ChannelRetrieveController::class, 'getUserSubscriptions']);
+        Route::post('{id}/subscribe', [ChannelSocialController::class, 'subscribe']);
+        Route::post('{id}/unsubscribe', [ChannelSocialController::class, 'unsubscribe']);
+        Route::post('{id}/create', [ChannelManageController::class, 'create']);
+        Route::post('{id}/delete', [ChannelManageController::class, 'delete']);
+    });
 });
